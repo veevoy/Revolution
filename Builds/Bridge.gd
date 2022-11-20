b597a18f16bdc3bf8d5e98b566ca848fa88c15e9
veevoy <veevoyel@gmail.com>
2022-11-20 02:20:49 -0500

    Added invisible wall to bridge to prevent skipping the sesame baguette

--- Builds/Bridge.gd
+++ Builds/Bridge.gd
@@ -2,7 +2,8 @@ extends Area2D
 
 onready var sprite = $AnimatedSprite
 onready var closed_collision = $ClosedCollision
-onready var open_collision = $StaticBody2D/OpenCollision
+onready var open_collision = $OpenStaticBody/OpenCollision
+onready var invisible_wall_collision = $InvisibleWall/InvisibleWallCollision
 var open = false
 
 func _on_Bridge_area_entered(area):
@@ -13,5 +14,6 @@ func _on_Bridge_area_entered(area):
 		set_collision_mask_bit(0, true)
 		set_collision_layer_bit(3, false)
 		closed_collision.set_deferred("disabled", true)
+		invisible_wall_collision.set_deferred("disabled", true)
 		open_collision.set_deferred("disabled", false)
 
