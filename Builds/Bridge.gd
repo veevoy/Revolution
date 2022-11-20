369c3e7f516ffd9cab5ee5d70dd9c5974aa85f39
veevoy <veevoyel@gmail.com>
2022-11-20 06:59:51 -0500

    Resolved conflicts with main

--- Builds/Bridge.gd
+++ Builds/Bridge.gd
@@ -8,12 +8,15 @@ onready var trigger_collision = $TriggerCollision
 onready var open_collision = $OpenStaticBody/OpenCollision
 onready var closed_collision = $ClosedStaticBody/ClosedCollision
 
+onready var sfx_triggered = $SFXTriggered
+
 var open = false
 
 func _on_Bridge_area_entered(area):
 	if not open and "hit_owner" in area and area.hit_owner == "sesame":
 		open = true
 		sprite.play("Open")
+		sfx_triggered.play()
 		set_collision_layer_bit(0, true)
 		set_collision_mask_bit(0, true)
 		set_collision_layer_bit(3, false)
